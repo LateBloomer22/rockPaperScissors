638b912b8ee9eb3de8995772feebe84cd3d1a2e6
LateBloomer22 <23632544+LateBloomer22@users.noreply.github.com>
2026-03-10 18:32:05 +0530

    First version of single round

---
+++ rockPaperScissors.js
@@ -0,0 +1,36 @@
+const possibleValues = ["rock", "paper", "scissor"];
+
+function getComputerChoice(){
+    let randomIndex = Math.floor(Math.random()*possibleValues.length);
+    return possibleValues[randomIndex];
+}
+
+function getHumanChoice(){
+    let input = prompt("What would you like to choose ? rock, paper or scissor ?")
+    return input;
+}
+
+function playRound(humanChoice, computerChoice ){
+    let result = "Human wins";
+    if (humanChoice == computerChoice) 
+    {
+        result = "It's a tie";
+    } 
+    else if (humanChoice == "rock") 
+    {
+        if (computerChoice == "paper"){result = "Computer wins"};
+    }
+
+    else if (humanChoice == "paper")
+    {
+        if (computerChoice == "scissor") {result = "Computer wins"};
+    }
+
+    else if (humanChoice == "scissor") 
+    {
+        if (computerChoice == "rock") {result = "Computer wins"};
+    }
+    console.log(`You chose ${humanChoice} & computer chose ${computerChoice} hence ${result}`);
+}
+
+playRound(getHumanChoice(),getComputerChoice());
