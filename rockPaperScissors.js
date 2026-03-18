db4e7121592270d2dfb9b108ffcbbeebc90dc3ff
LateBloomer22 <23632544+LateBloomer22@users.noreply.github.com>
2026-03-18 15:08:47 +0530

    Added basic 5 round checker

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,6 +2,10 @@ let humanScore = 0;
 let computerScore = 0;
 let tieRounds = 0;
 const possibleValues = ["rock", "paper", "scissor"];
+let outputText = document.querySelector(".result");
+let outputScore = document.querySelector(".score");
+let winner = document.querySelector(".winner");
+let timer = 2;
 
 function getComputerChoice(){
     let randomIndex = Math.floor(Math.random()*possibleValues.length);
@@ -22,18 +26,44 @@ function playRound(humanChoice, computerChoice){
 }
 
 function myFunc(humanChoice){
-    let computerChoice = getComputerChoice();
-    let roundResult = playRound(humanChoice, computerChoice);
-    // if (roundResult == "win"){
-    //     humanScore += 1;        
-    // } 
-    // else if (roundResult == "lose"){
-    //     computerScore += 1;
-    // } 
-    // else if (roundResult == "tie"){
-    //     tieRounds += 1;
-    // }
-    console.log(`You ${roundResult}`);
+    if (humanScore <= 4 && computerScore <= 4){    
+        let computerChoice = getComputerChoice();
+        let roundResult = playRound(humanChoice, computerChoice);
+        if (roundResult == "win"){
+            humanScore += 1;        
+        } 
+        else if (roundResult == "lose"){
+            computerScore += 1;
+        } 
+        else if (roundResult == "tie"){
+            tieRounds += 1;
+        }
+        
+        outputText.textContent =`You ${roundResult} this round`;
+        outputScore.textContent = `Score after this round is: 
+        Player = ${humanScore} / Computer = ${computerScore} / 
+        Tie-round = ${tieRounds}`;
+    }
+    // Winner announce if anyone reaches a score of 5
+    else if (humanScore == 5 || computerScore == 5) {
+        if (humanScore > computerScore) {
+            winner.textContent = `You have won this game. The game will automatically reset after ${timer} seconds`;
+        } else if (humanScore < computerScore){
+            winner.textContent = `You lost this game. The game will automatically reset after ${timer} seconds`;
+        }        
+        setTimeout(resetFunc, 2000)
+
+    }
+}
+
+function resetFunc(){
+    humanScore = 0;
+    computerScore = 0;
+    tieRounds = 0;
+    outputText.textContent = "";
+    outputScore.textContent = "";
+    winner.textContent = "";
+    alert("The game has been reset");
 }
 
 let choices = document.querySelectorAll("button.choice");
@@ -42,3 +72,6 @@ choices.forEach((choice) => {
         myFunc(choice.id);
     })
 });
+
+let resetGame = document.querySelector(".reset");
+resetGame.addEventListener("click", () => {resetFunc()});
